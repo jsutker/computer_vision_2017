c658e29a0dc4ef4037d37d3c5879308057311fd0
Joseph Sutker <joseph.sutker@students.olin.edu>
2017-03-27 16:44:43 -0400

    coordinate_translator works(?)

--- pacneato/scripts/coordinate_translator.py
+++ pacneato/scripts/coordinate_translator.py
@@ -1,13 +1,18 @@
+import numpy as np
+from tf.transformations import rotation_matrix
 def coordinate_translator(world_coords, odom_coords, focal_point, principal_point):
-  w_x, w_y, w_z = world_coords
+  w_z, w_x, w_y = world_coords
   o_x, o_y, o_theta = odom_coords
   fx, fy = focal_point
   cx, cy = principal_point
 
-  t = (_,_,_)
-  rx = (_,_,_)
-  ry = (_,_,_)
-  rz = (_,_,_)
+  t = (w_x-o_x, w_y-o_y, w_z)
+
+  r = rotation_matrix(o_theta, (0,0,1))
+
+  rx = r[0][:3]
+  ry = r[1][:3]
+  rz = r[2][:3]
 
   x = t[0] + (w_x*rx[0]) + (w_y*rx[1]) + (w_z*rx[2])
   y = t[1] + (w_x*ry[0]) + (w_y*ry[1]) + (w_z*ry[2])
