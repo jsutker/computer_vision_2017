5a3d38e1ee3aeea705662c522ee62c9a03460993
buttegab <gabriel.butterick@students.olin.edu>
2017-03-30 17:01:34 -0400

    now makes the warped image transparent where added blackness appears

--- pacneato/scripts/placeImage.py
+++ pacneato/scripts/placeImage.py
@@ -3,10 +3,22 @@ import numpy as np
 def placeImage (image,pts2):	
 	"""Takes an image as input as well as the desired perspective of the image.
 	Returns the transformed version of the image."""
-	
+	image = cv2.imread(img,-1)
 	width = len(image[0])
 	length = len(image)
 	pts1 = [[0,0],[0,width],[length,width],[length,0]]
 	M = cv2.getPerspectiveTransform(pts1,pts2)  #find homography instead?
 	dst = cv2.warpPerspective(image,M,(length,width))
-	return dst
+	return cropImage(dst)
+
+def cropImage(image):
+	for i in image:
+		for x in i:
+			if x[0] == 0 and x[1] == 0 and x[2] == 0:
+				x[3] = 0
+	return image
+
+
+if __name__ == '__main__':
+	image = "./test.png"
+	cropImage(image)
